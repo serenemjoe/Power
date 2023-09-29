c4a39bba90c09706db90d96e4e7cf7d39429f448
serenemjoe <122944947+serenemjoe@users.noreply.github.com>
2023-09-29 12:07:10 -0400

    Add files via upload
    Initilializing Odrive code

---
+++ odrivecode.py
@@ -0,0 +1,8 @@
+# Serene, Camille, Lorina, Noam, Lamiya
+# Hyperloop Power and Propulsion 2023
+# O-Drive Configuration Code
+
+import odrive
+odrv0 = odrive.find_any()
+print(str(ordv0.vbus_voltage))
+
